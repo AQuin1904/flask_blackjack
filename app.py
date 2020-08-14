2fa70ead90274685ac67269b1271666a28ce5776
William Varteresian <william.varteresian@gmail.com>
2020-08-14 13:49:14 -0400

    Enables hit and deal on /play route
    Enables hitting and dealing in a persistent game on the /play route using query strings to identify player action. TO DO: Display player hand value, implement win conditions, implement dealer hitting or standing, conceal first card in dealer's hand from user.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from Blackjack import Blackjack
 app = Flask(__name__)
 
-def game():
-    bj = Blackjack()
-    yield bj
-    bj.deal()
-    yield bj
+bj = Blackjack()
 
 @app.route('/')
 def index():
@@ -14,9 +10,20 @@ def index():
 
 @app.route('/play')
 def play():
-    bj = next(game())
+    global bj
+    next = request.args.get('next', '')
+    if next == '':
+        action = 'deal'
+    elif next == 'deal':
+        bj.deal()
+        action = 'hit'
+    elif next == 'hit':
+        bj.hit(bj.player)
+        action = 'hit'
+    else:
+        action = 'hit'
     return render_template('play.html',
                            dealer_hand=bj.dealer.hand,
                            player_hand=bj.player.hand,
-                           action='deal'
+                           action=action
                           )
