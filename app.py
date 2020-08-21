547d8f33f7301bc5b73f71a066a35527edc2c797
William Varteresian <william.varteresian@gmail.com>
2020-08-20 20:00:38 -0400

    improves card display
    Implements a function to process player and dealer hands to HTML. Improves CSS for styling hand and card elements. TO DO: improve total_hand function in Blackjack class to correctly handle multiple aces, perform full unit testing, implement support for multiple distinct sessions.

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ app = Flask(__name__)
 
 bj = Blackjack()
 
+def cards_to_display(dealer, player, winner):
+    card_t = ('<div class="card">{rank} of '
+             + '<span class="{suit}">{suit}</span></div>')
+    dealer_hand = ''
+    player_hand = ''
+    if len(dealer.hand) > 0 and not winner:
+        dealer_hand += card_t.format(rank = '?', suit = '?')
+        for card in dealer.hand[1:]:
+            dealer_hand += card_t.format(rank = card.rank, suit = card.suit)
+    else:
+        for card in dealer.hand:
+            dealer_hand += card_t.format(rank = card.rank, suit = card.suit)
+    for card in player.hand:
+        player_hand += card_t.format(rank = card.rank, suit = card.suit)
+    return dealer_hand, player_hand
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -31,6 +47,7 @@ def play():
     else:
         action = (action_t.format(act='hit')
                  + action_t.format(act='stand'))
+
     winner = bj.check_winner()
     if winner == bj.dealer:
         action = ('<h3>Dealer wins!<br>Play again?</h3>'
@@ -38,8 +55,10 @@ def play():
     elif winner == bj.player:
         action = ('<h3>You win!<br>Play again?</h3>'
                  + action_t.format(act='deal'))
+
+    dealer_hand, player_hand=cards_to_display(bj.dealer, bj.player, winner)
     return render_template('play.html',
-                           dealer_hand=bj.dealer.hand,
-                           player_hand=bj.player.hand,
+                           dealer_hand=dealer_hand,
+                           player_hand=player_hand,
                            action=action
                           )
